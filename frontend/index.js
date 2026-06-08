e7f71fb5cd0dd0e8f840b00b0c18b81ffe77a29f
enzoronchii <enzoronchi413@gmail.com>
2026-06-08 17:15:23 -0300

    vercel3

--- frontend/index.js
+++
@@ -1,4 +0,0 @@
-import { Header } from './src/components/header/index.js';
-
-// Central Registration of Web Components
-customElements.define('app-header', Header);
---
+++ frontend/src/components/header/export.js
@@ -0,0 +1,1 @@
+export { HeaderComponent as Header } from './export.js';
