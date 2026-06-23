9dd69a88a600c81fef1e826ec1d18b6592c40e13
BeatrizCercal <biaccachoeira@gmail.com>
2026-06-23 13:05:46 -0300

    feat: alt user photo

--- frontend/public/src/components/header/header.js
+++ frontend/public/src/components/header/header.js
@@ -1,3 +1,6 @@
+const PROFILE_IMAGE_KEY = 'biosearch-profile-image';
+const DEFAULT_PROFILE_IMAGE = '../src/assets/user-icon.png';
+
 const template = document.createElement('template');
 
 template.innerHTML = `
@@ -13,7 +16,7 @@ template.innerHTML = `
 
     <div class="user-icon">
       <a href="user.html">
-        <img src="../src/assets/user-icon.png" alt="User Profile">
+        <img src="${DEFAULT_PROFILE_IMAGE}" alt="User Profile">
       </a>
     </div>
   </nav>
@@ -46,20 +49,39 @@ class HeaderComponent extends HTMLElement {
     this.shadowRoot.appendChild(template.content.cloneNode(true));
 
     this.handleMenuClick = this.handleMenuClick.bind(this);
+    this.handleProfileImageChange = this.handleProfileImageChange.bind(this);
   }
 
   connectedCallback() {
     this.menuIcon = this.shadowRoot.querySelector('.menu-icon');
     this.sidebar = this.shadowRoot.querySelector('.sidebar');
     this.closeMenu = this.shadowRoot.querySelector('.close-menu');
+    this.profileImage = this.shadowRoot.querySelector('.user-icon img');
 
     this.menuIcon.addEventListener('click', this.handleMenuClick);
     this.closeMenu.addEventListener('click', this.handleMenuClick);
+    window.addEventListener('storage', this.handleProfileImageChange);
+    window.addEventListener('profile-image-changed', this.handleProfileImageChange);
+
+    this.updateProfileImage();
   }
 
   disconnectedCallback() {
     this.menuIcon.removeEventListener('click', this.handleMenuClick);
     this.closeMenu.removeEventListener('click', this.handleMenuClick);
+    window.removeEventListener('storage', this.handleProfileImageChange);
+    window.removeEventListener('profile-image-changed', this.handleProfileImageChange);
+  }
+
+  updateProfileImage() {
+    if (!this.profileImage) return;
+
+    const savedImage = localStorage.getItem(PROFILE_IMAGE_KEY);
+    this.profileImage.src = savedImage || DEFAULT_PROFILE_IMAGE;
+  }
+
+  handleProfileImageChange() {
+    this.updateProfileImage();
   }
 
   handleMenuClick() {
--- frontend/public/src/js/user.js
+++ frontend/public/src/js/user.js
@@ -1,6 +1,29 @@
+const PROFILE_IMAGE_KEY = 'biosearch-profile-image';
+const DEFAULT_PROFILE_IMAGE = '../src/assets/user-icon.png';
+
+function updateHeaderProfileImage(imageSrc) {
+    const header = document.querySelector('app-header');
+
+    if (!header?.shadowRoot) return;
+
+    const headerImage = header.shadowRoot.querySelector('.user-icon img');
+
+    if (headerImage) {
+        headerImage.src = imageSrc;
+    }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     const profileImage = document.getElementById('profileImage');
     const fileInput = document.getElementById('fileInput');
+    const savedProfileImage = localStorage.getItem(PROFILE_IMAGE_KEY);
+
+    if (savedProfileImage) {
+        profileImage.src = savedProfileImage;
+        updateHeaderProfileImage(savedProfileImage);
+    } else {
+        profileImage.src = DEFAULT_PROFILE_IMAGE;
+    }
 
     profileImage.addEventListener('click', () => {
         fileInput.click();
@@ -14,7 +37,10 @@ document.addEventListener('DOMContentLoaded', () => {
         const reader = new FileReader();
 
         reader.onload = (e) => {
-            profileImage.src = e.target.result;
+            const imageSrc = e.target.result;
+            profileImage.src = imageSrc;
+            localStorage.setItem(PROFILE_IMAGE_KEY, imageSrc);
+            updateHeaderProfileImage(imageSrc);
         };
 
         reader.readAsDataURL(file);
